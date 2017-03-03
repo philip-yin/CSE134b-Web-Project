68dac8a62748940e2cc9c5cde04f7858e9534958
philip yin <jiy114@ucsd.edu>
2017-03-03 13:34:38 -0800

    work done 3/3 meeting

---
+++ HW4/wireframes/javascript/common.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAIWLbugKRBWSFmyEzgD_HPYdSFOEhztyk",
+    authDomain: "leaguedex.firebaseapp.com",
+    databaseURL: "https://leaguedex.firebaseio.com",
+    storageBucket: "leaguedex.appspot.com",
+    messagingSenderId: "47559996849"
+  };
+  firebase.initializeApp(config);
