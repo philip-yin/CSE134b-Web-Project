07fe7cc7e60a6436c216006a1d335c31b7f27259
philip yin <jiy114@ucsd.edu>
2017-03-04 22:09:21 -0800

    minor change

--- HW4/wireframes/javascript/signup.js
+++ HW4/wireframes/javascript/signup.js
@@ -1,3 +1,7 @@
 	window.onload = function() {
       document.getElementById('signup').addEventListener('click', handleSignUp, false);
+	  location.reload();
+	  if (firebase.auth().currentUser){
+		  alert("Sign up successful");
+	  }
     };
