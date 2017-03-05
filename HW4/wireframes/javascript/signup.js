3120440f863325ae0d47b9593cba4d7d2cd7e3b2
Steve Wang <steve_wang3@yahoo.com>
2017-03-04 23:56:33 -0800

    minor change

--- HW4/wireframes/javascript/signup.js
+++ HW4/wireframes/javascript/signup.js
@@ -1,6 +1,5 @@
 	window.onload = function() {
       document.getElementById('signup').addEventListener('click', handleSignUp, false);
-	  location.reload();
 	  if (firebase.auth().currentUser){
 		  alert("Sign up successful");
 	  }
