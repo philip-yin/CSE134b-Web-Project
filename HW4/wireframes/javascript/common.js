c725021f19f4f15668bd6296c5c60b1a4a3d8b8b
philip yin <jiy114@ucsd.edu>
2017-03-04 18:18:20 -0800

    add folder for css and other changes

--- HW4/wireframes/javascript/common.js
+++ HW4/wireframes/javascript/common.js
@@ -1,9 +1,62 @@
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAIWLbugKRBWSFmyEzgD_HPYdSFOEhztyk",
-    authDomain: "leaguedex.firebaseapp.com",
-    databaseURL: "https://leaguedex.firebaseio.com",
-    storageBucket: "leaguedex.appspot.com",
-    messagingSenderId: "47559996849"
-  };
-  firebase.initializeApp(config);
+	var config = {
+		apiKey: "AIzaSyAIWLbugKRBWSFmyEzgD_HPYdSFOEhztyk",
+		authDomain: "leaguedex.firebaseapp.com",
+		databaseURL: "https://leaguedex.firebaseio.com",
+		storageBucket: "leaguedex.appspot.com",
+		messagingSenderId: "47559996849"
+	};
+	firebase.initializeApp(config);
+  
+	window.onload = function() {
+      initApp();
+    };
+	
+	function initApp() {
+      // Listening for auth state changes.
+      // [START authstatelistener]
+      firebase.auth().onAuthStateChanged(function(user) {
+        // [START_EXCLUDE silent]
+        document.getElementById('quickstart-verify-email').disabled = true;
+        // [END_EXCLUDE]
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // [START_EXCLUDE]
+          document.getElementById('status').textContent = 'Signed in';
+          document.getElementById('login').textContent = 'Sign out';
+          document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+          if (!emailVerified) {
+            document.getElementById('quickstart-verify-email').disabled = false;
+          }
+          // [END_EXCLUDE]
+        } else {
+          // User is signed out.
+          // [START_EXCLUDE]
+          document.getElementById('status').textContent = 'Signed out';
+          document.getElementById('login').textContent = 'Sign in';
+          document.getElementById('quickstart-account-details').textContent = 'null';
+          // [END_EXCLUDE]
+        }
+        // [START_EXCLUDE silent]
+        document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+      });
+      // [END authstatelistener]
+
+      document.getElementById('login').addEventListener('click', toggleSignIn, false);
+      document.getElementById('signup').addEventListener('click', handleSignUp, false);
+      document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
+      document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
+    }
+	
+	
+	
+	
+	
