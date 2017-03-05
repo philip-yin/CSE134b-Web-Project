f1c355f1170b81c25826ae5d061a1e11d6597624
philip yin <jiy114@ucsd.edu>
2017-03-05 15:25:57 -0800

    add google sign in method

--- HW4/wireframes/javascript/login.js
+++ HW4/wireframes/javascript/login.js
@@ -74,6 +74,51 @@
         // [END authwithemail]
       }
     }
+	
+	function googleSignIn() {
+      if (!firebase.auth().currentUser) {
+        // [START createprovider]
+        var provider = new firebase.auth.GoogleAuthProvider();
+        // [END createprovider]
+        // [START addscopes]
+        provider.addScope('https://www.googleapis.com/auth/plus.login');
+        // [END addscopes]
+        // [START signin]
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // [START_EXCLUDE]
+          //document.getElementById('quickstart-oauthtoken').textContent = token;
+          // [END_EXCLUDE]
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/account-exists-with-different-credential') {
+            alert('You have already signed up with a different auth provider for that email.');
+            // If you are using multiple auth providers on your app you should handle linking
+            // the user's accounts here.
+          } else {
+            console.error(error);
+          }
+          // [END_EXCLUDE]
+        });
+        // [END signin]
+      } else {
+        // [START signout]
+		alert("you already signed in with an account!");
+        // [END signout]
+      }
+      // [START_EXCLUDE]
+      // [END_EXCLUDE]
+    }
 
 	function checkStatus(){
 		firebase.auth().onAuthStateChanged(function(user) {
@@ -132,6 +177,8 @@
 	  document.getElementById('login').addEventListener('click', toggleSignIn, false);
 	  document.getElementById('inout').addEventListener('click', signout, false);
 	  document.getElementById('login').addEventListener('click', checkStatus, false);
+	  document.getElementById('google').addEventListener('click', googleSignIn, false);
+	  document.getElementById('google').addEventListener('click', checkStatus, false);
     }
 	
 	
